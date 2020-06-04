e61edca8a66597af20bade5655f7179e0bb47913
Cristobal <cristobal.medrano@usach.cl>
2020-06-04 01:41:02 -0400

    validate getopt args

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <ctype.h>
 #include <jpeglib.h>
 #include <getopt.h>
 #include <string.h>
@@ -10,6 +11,29 @@
 #include <menu.h>
 #include <pipeline.h>
 
+int isInteger(char* optarg, char* type)
+{
+    int i = 0;
+    char dot = '.';
+    char comma = ',';
+    char dash = '-';
+    if (optarg != NULL){
+        for (i = 0; i < strlen(optarg); i++)
+        {
+            if (optarg[i] == dot || 
+                optarg[i] == comma ||
+                optarg[i] == dash ||
+                isalpha(optarg[i])){
+                printf("Error. El valor para %s debe ser un entero positivo.\n", type);
+                return FALSE;
+            }
+        }
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
 int main(int argc, char *argv[]) {
     int numberImages = 0;
     char* maskFilename = NULL;
@@ -17,31 +41,45 @@ int main(int argc, char *argv[]) {
     int binarizationThreshold = 0;
     int flagShowResults = 0;
     int opt = 0;
-    while ((opt = getopt (argc, argv, "c:u:n:m:b")) != -1)
+    while ((opt = getopt (argc, argv, "c:u:n:m:b::")) != -1)
     {
       switch (opt){
         case 'c':
+            if (!isInteger(optarg, "la cantidad de imagenes")) exit(EXIT_FAILURE);
             numberImages = atoi(optarg);		  //cantidad de imágenes.
-            printf("Cantidad imagenes: %d\n", numberImages);
+            //printf("Cantidad imagenes: %d\n", numberImages);
             break;
         case 'u':
+            if (!isInteger(optarg, "el umbral de binarizacion")) exit(EXIT_FAILURE);
             binarizationThreshold = atoi(optarg);	      //umbral binarizar la imagen.
-            printf("Umbral para binarizar: %d\n", binarizationThreshold);
+            //printf("Umbral para binarizar: %d\n", binarizationThreshold);
             break;
         case 'n':
+            if (!isInteger(optarg, "el umbral de clasificacion")) exit(EXIT_FAILURE);
             classificationThreshold = atoi(optarg);	      //umbral clasificacion.
-            printf("Umbral de clasificacion: %d\n", classificationThreshold);
+            //printf("Umbral de clasificacion: %d\n", classificationThreshold);
             break;
         case 'm':
             maskFilename = (char*)malloc(sizeof(char)*40);
             maskFilename = strcpy(maskFilename, optarg);		      //máscara.
-            printf("Mascara: %s\n", maskFilename);
+            //printf("Mascara: %s\n", maskFilename);
             break;
         case 'b':
             flagShowResults = 1;				     //bandera.
-            printf("Bandera: %d\n", flagShowResults);
+            //printf("Bandera: %d\n", flagShowResults);
             break;
+        /*case '?':
+            if(optopt == 'c' || optopt == 'u' || optopt == 'n' || optopt == 'm'){
+                fprintf(stderr, "Opcion -%c requiere un argumento.\n", optopt);
+                exit(EXIT_FAILURE);}
+            else {
+                fprintf(stderr, "Opcion -%c desconocida.\n", optopt);
+                exit(EXIT_FAILURE);}
+            break;*/
         default:
+            fprintf(stderr, "Uso: %s [-c cantidad de imagenes] [-u umbral binarizacion] [-n umbral clasificacion] [-m mascara] [-b]\n",
+            argv[0]);
+            exit(EXIT_FAILURE);
             break;
       }
     }
